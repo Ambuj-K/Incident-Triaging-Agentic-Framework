da5b62eb88a2300ea9fa74977adc7a10d60a8cf2
Ambuj-K <ambuj@cooper.ai>
2026-05-05 10:14:00 +0530

    minor langfuse fix

--- scripts/check_langfuse.py
+++ scripts/check_langfuse.py
@@ -7,7 +7,7 @@ load_dotenv()
 lf = Langfuse(
     public_key=os.environ["LANGFUSE_PUBLIC_KEY"],
     secret_key=os.environ["LANGFUSE_SECRET_KEY"],
-    host=os.environ["LANGFUSE_HOST"],
+    host=os.environ["LANGFUSE_BASE_URL"],
 )
 
 print(lf.auth_check())
